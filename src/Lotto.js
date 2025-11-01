26ea406e098ccc881d89f173b8914e35b24a56b3
lee-eojin <lejin0906@snu.ac.kr>
2025-11-01 21:20:48 +0900

    feat(Lotto): complete Lotto class with validation

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,37 @@
 class Lotto {
-  #numbers;
-
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.numbers = numbers;
+  }
+
+  getLottoNumber() {
+    return this.numbers;
+  }
+
+  getCorrectNumber(targetNumbers) {
+    return this.numbers.filter(number => targetNumbers.includes(number)).length;
+  }
+
+  hasBonusNumber(bonusNumber) {
+    return this.numbers.includes(bonusNumber);
   }
 
   #validate(numbers) {
+    this.#validateLength(numbers);
+    this.#validateDuplicate(numbers);
+  }
+
+  #validateLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
 
-  // TODO: 추가 기능 구현
+  #validateDuplicate(numbers) {
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }
+  }
 }
 
 export default Lotto;
